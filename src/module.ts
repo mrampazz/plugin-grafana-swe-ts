548c2325564fb67a89795c40879e0825206311d1
mrampazz <mrampazz@gmail.com>
2020-02-27 23:13:21 +0100

    Tradotto pagine da React in Angular, per problemi di compatibilità

--- src/module.ts
+++ src/module.ts
@@ -2,25 +2,13 @@ import { AppPlugin } from '@grafana/data';
 import { AppSettings } from './types';
 import { ConfigCtrl } from './config/config';
 
-import { RootPage } from './pages/RootPage';
 import { Page1 } from './pages/Page1';
 import { Page2 } from './pages/Page2';
 
 export {
-  ConfigCtrl
+  ConfigCtrl,
+  Page1,
+  Page2
 };
 
-export const plugin = new AppPlugin<AppSettings>()
-  .setRootPage(RootPage)
-  .addConfigPage({
-    title: 'Page 1',
-    icon: 'fa fa-info',
-    body: Page1,
-    id: 'page1',
-  })
-  .addConfigPage({
-    title: 'Page 2',
-    icon: 'fa fa-user',
-    body: Page2,
-    id: 'page2',
-  });
+export const plugin = new AppPlugin<AppSettings>();
---
+++ src/pages/Page2.ts
@@ -0,0 +1,8 @@
+export class Page2 {
+    static templateUrl = 'pages/page2.html';
+
+    /** @ngInject */
+    constructor($scope: any, $rootScope: any) {
+        console.log("angular u suck");
+    }
+}

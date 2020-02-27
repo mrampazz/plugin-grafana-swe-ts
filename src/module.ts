0265eabcf01737ab7e61748019e98bb17d953667
mrampazz <mrampazz@gmail.com>
2020-02-27 11:25:48 +0100

    Closes #2 aggiunta pagina per abilitare il plugin

--- src/module.ts
+++ src/module.ts
@@ -1,11 +1,14 @@
 import { AppPlugin } from '@grafana/data';
 import { AppSettings } from './types';
-// import { ConfigCtrl } from './config/config';
-// legacy imports not needed for new stuff??????
+import { ConfigCtrl } from './config/config';
 
-import { RootPage } from './config/RootPage';
-import { Page1 } from './config/Page1';
-import { Page2 } from './config/Page2';
+import { RootPage } from './config/react/RootPage';
+import { Page1 } from './config/react/Page1';
+import { Page2 } from './config/react/Page2';
+
+export {
+  ConfigCtrl
+};
 
 export const plugin = new AppPlugin<AppSettings>()
   .setRootPage(RootPage)

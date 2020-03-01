d0601db3647447609f3df451a97b5e96c97de79c
mrampazz <mrampazz@gmail.com>
2020-03-01 14:13:30 +0100

    Fixed error uknown provider + added commented out code for enabling plugin

--- src/components/config.ts
+++ src/components/config.ts
@@ -5,12 +5,34 @@ export class ConfigCtrl {
   appModel: any;
   $location: any;
 
+  /** @ngInject */
   constructor($location: any) {
     this.$location = $location;
     this.enabled = false;
     this.appEditCtrl = null;
     this.appModel = null;
+
+    // Codice consigliato da Grafana per gestire abilitazione plugin
+    // this.appEditCtrl.setPostUpdateHook(this.postUpdate.bind(this));
+    // if (!this.appModel) {
+    //   this.appModel = {} as PluginMeta;
+    // }
+
+    // const appModel = this.appModel as any;
+    // if (!this.appModel.jsonData) {
+    //   this.appModel.jsonData = {};
+    // }
+    // console.log(this);
   }
+
+  // postUpdate() {
+  //   if (!this.appModel.enabled) {
+  //     console.log("plugin disabled");
+  //     return;
+  //   }
+
+  //   console.log('Post Update, plugin loaded', this);
+  // }
 
   redirect() {
     //console.info("redirect() not done");

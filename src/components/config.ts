67e48e77198f6d1961c8acc987c64352ddc0ba24
mrampazz <mrampazz@gmail.com>
2020-03-01 14:31:07 +0100

    Fixed buttons enable/disable/update

--- src/components/config.ts
+++ src/components/config.ts
@@ -1,38 +1,39 @@
+import { PluginMeta } from '@grafana/data';
 export class ConfigCtrl {
   static templateUrl: string;
-  enabled: boolean;
+  // enabled: boolean;
   appEditCtrl: any;
-  appModel: any;
+  appModel?: PluginMeta;
   $location: any;
 
   /** @ngInject */
   constructor($location: any) {
     this.$location = $location;
-    this.enabled = false;
-    this.appEditCtrl = null;
-    this.appModel = null;
+    // this.enabled = false;
+    // this.appEditCtrl = null;
+    // this.appModel = null;
 
     // Codice consigliato da Grafana per gestire abilitazione plugin
-    // this.appEditCtrl.setPostUpdateHook(this.postUpdate.bind(this));
-    // if (!this.appModel) {
-    //   this.appModel = {} as PluginMeta;
-    // }
+    this.appEditCtrl.setPostUpdateHook(this.postUpdate.bind(this));
+    if (!this.appModel) {
+      this.appModel = {} as PluginMeta;
+    }
 
-    // const appModel = this.appModel as any;
-    // if (!this.appModel.jsonData) {
-    //   this.appModel.jsonData = {};
-    // }
-    // console.log(this);
+    const appModel = this.appModel as any;
+    if (!appModel.jsonData) {
+      appModel.jsonData = {};
+    }
+    console.log(this);
   }
 
-  // postUpdate() {
-  //   if (!this.appModel.enabled) {
-  //     console.log("plugin disabled");
-  //     return;
-  //   }
+  postUpdate() {
+    if (!this.appModel?.enabled) {
+      console.log("plugin disabled");
+      return;
+    }
 
-  //   console.log('Post Update, plugin loaded', this);
-  // }
+    console.log('Post Update, plugin loaded', this);
+  }
 
   redirect() {
     //console.info("redirect() not done");

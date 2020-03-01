03ac166ea735b458926267cd5da34e85bb8c7b92
mrampazz <mrampazz@gmail.com>
2020-03-01 16:16:37 +0100

    Aggiunta conferma rimozione JSON, aggiunto button rimozione JSON dopo caricamento

--- src/panels/import-json-panel/JsonImportPanel.ts
+++ src/panels/import-json-panel/JsonImportPanel.ts
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import { PanelEvents } from '@grafana/data';
 import { PanelCtrl } from 'grafana/app/plugins/sdk';
-import appEvents from 'grafana/app/core/app_events';
+import { appEvents } from 'grafana/app/core/core';
 
 export class JsImportPanel extends PanelCtrl {
   static templateUrl = 'panels/import-json-panel/partials/panelTemplate.html';
@@ -16,7 +16,6 @@ export class JsImportPanel extends PanelCtrl {
   constructor($scope: any, $injector: any) {
     super($scope, $injector);
     _.defaults(this.panel, this.panelDefaults);
-
     this.events.on(PanelEvents.editModeInitialized, this.onInitEditMode.bind(this));
   }
 
@@ -27,12 +26,18 @@ export class JsImportPanel extends PanelCtrl {
   // Called only on import button click, if the import doesn't throw errors, it reset the saved data
   async upload_button_click(net: any) {
     await this.onUpload(net);
-
-    appEvents.emit('alert-success', ['JSON file imported!', 'Success']);
   }
 
   // Called on import button click but also to re-load a saved network
-  async onUpload(net: any) {}
+  async onUpload(net: any) {
+    this.panel.jsonContent = JSON.stringify(net, null, "\t");
+    appEvents.emit('alert-success', ['File Json Caricato']);
+  }
+
+  async delete_json_click() {
+    this.panel.jsonContent = null;
+    appEvents.emit('alert-success', ['File Json Cancellato']);
+  }
 
   // Called from anularjs with ng-change
   onTextBoxRefresh() {}
